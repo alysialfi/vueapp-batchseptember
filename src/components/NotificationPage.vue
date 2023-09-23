c8452fef5b86256833e142fbb2724279cf2926a4
alysialfi <9c.alysialfi@gmail.com>
2023-09-23 13:46:47 +0700

    feat: (NotificationPage.vue) modify style & add modal layout

--- src/components/NotificationPage.vue
+++ src/components/NotificationPage.vue
@@ -35,17 +35,33 @@ export default {
             ],
         }
     },
+    methods: {
+        addNotification() {
+            const newNotif = {
+                name: this.notificationBody.name,
+                action: this.notificationBody.action,
+                message: this.notificationBody.message,
+                time: '1 sec ago',
+                hasRead: false
+            }
+            this.notifs.push(newNotif)
+            this.isModalOpen = false
+            for (const body in this.notificationBody) {
+                this.notificationBody[body] = ''
+            }
+        }
+    },
 }
 </script>
 
 <template>
-    <div class="w-screen flex overflow-auto">
-        <div class="w-6/12 mx-auto h-screen bg-white shadow-lg px-12 py-10">
+    <div class="relative w-screen flex">
+        <div class="relative w-6/12 mx-auto h-screen bg-white shadow-lg px-12 py-10 overflow-auto">
             <!-- header -->
             <div class="flex justify-between mb-6 items-end">
                 <div class="flex gap-1 items-center">
                     <h1 class="font-bold text-lg">Notification</h1>
-                    <span class="px-2 bg-blue-900 text-white font-semibold rounded-md text-center">3</span>
+                    <span class="px-2 bg-blue-900 text-white font-semibold rounded-md text-center">{{ notifs.length }}</span>
                 </div>
                 <span class="text-sm text-gray-500">Mark all as read</span>
             </div>
@@ -71,6 +87,39 @@ export default {
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <!-- button add -->
+        <div class="absolute bottom-4 right-[27%]">
+            <button id="show-btn" @click="isModalOpen = true" class="w-10 h-10 rounded-full bg-orange-400 text-white font-semibold text-3xl flex justify-center items-center hover:bg-orange-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>  
+            </button>
+        </div>
+
+        <!-- modal -->
+        <div v-if="isModalOpen" class="absolute flex items-center translate-x-1/2 h-screen w-6/12 bg-gray-500 bg-opacity-30">
+        <!-- <div :class="isModalOpen ? 'block' : 'hidden'" class="absolute flex items-center translate-x-1/2 h-screen w-6/12 bg-gray-500 bg-opacity-30"> -->
+            <div id="modal" class="w-full bg-white rounded-md mx-8 p-4 flex flex-col gap-2">
+                <div class="flex justify-between items-start">
+                    <span class="mb-2 font-semibold">Add Notification</span>
+                    <button  @click="isModalOpen = false">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="text-red-500 w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+                <input v-model="notificationBody.name" type="text" id="name" class="w-full h-10 p-2 rounded-md border-2 border-gray-200" placeholder="Name">
+                <select v-model="notificationBody.action" name="action" id="action" class="w-full h-10 p-2 rounded-md border-2 bg-white border-gray-200">
+                    <option value="" selected>Choose action</option>
+                    <option value="join">Join</option>
+                    <option value="follow">Follow</option>
+                    <option value="message">Message</option>
+                </select>
+                <textarea v-if="notificationBody.action === 'message'" v-model="notificationBody.message" name="message" id="message" placeholder="Write your message" class="w-full h-36 p-2 rounded-md border-2 border-gray-200"></textarea>
+                <button @click="addNotification" id="add-btn" class="w-16 py-1 text-white bg-orange-400 rounded-md mt-2 hover:bg-orange-500">Add</button>
             </div>
         </div>
     </div>

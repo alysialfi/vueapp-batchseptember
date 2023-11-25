63d45e9280ee63ff9410c702a441fd46c8fa0155
alysialfi <9c.alysialfi@gmail.com>
2023-11-25 14:35:43 +0700

    feat: add notifivcation page test cases

--- __test__/notificationPage.spec.js
+++ __test__/notificationPage.spec.js
@@ -3,6 +3,9 @@ import NotificationPage from '../src/components/NotificationPage.vue'
 
 describe('NotificationPage.vue Unit Test', () => {
     const wrapper = mount(NotificationPage)
+    // const items = wrapper.findAll('.notif-list')
+    // console.log(wrapper.vm.notifs.length);
+    // console.log(wrapper.find('#name'));
 
     it('should have Notification as a title', () => {
         expect(wrapper.text()).toMatch('Notification')
@@ -10,5 +13,11 @@ describe('NotificationPage.vue Unit Test', () => {
     it('should open notification modal', async () => {
         await wrapper.find('#show-btn').trigger('click')
         expect(wrapper.find('#modal').text()).toMatch('Add Notification')
+    });
+    it('should be added 1 new notification', async () => {
+        await wrapper.find('#show-btn').trigger('click')
+        wrapper.find('#name').setValue('Alfi')
+        wrapper.find('#add-btn').trigger('click')
+        expect(wrapper.vm.notifs.length).toBe(5)
     });
 })

6be0d138075e88382d8101cd9cdccc9db47df289
alysialfi <9c.alysialfi@gmail.com>
2023-12-16 14:06:51 +0700

    fix(test/notificationPage): error on 3rd test case

--- __test__/notificationPage.spec.js
+++ __test__/notificationPage.spec.js
@@ -1,23 +1,20 @@
-import { mount } from '@vue/test-utils'
-import NotificationPage from '../src/components/NotificationPage.vue'
+import { mount } from '@vue/test-utils';
+import NotificationPage from '../src/components/NotificationPage.vue';
 
 describe('NotificationPage.vue Unit Test', () => {
-    const wrapper = mount(NotificationPage)
-    // const items = wrapper.findAll('.notif-list')
-    // console.log(wrapper.vm.notifs.length);
-    // console.log(wrapper.find('#name'));
+  const wrapper = mount(NotificationPage);
 
-    it('should have Notification as a title', () => {
-        expect(wrapper.text()).toMatch('Notification')
-    })
-    it('should open notification modal', async () => {
-        await wrapper.find('#show-btn').trigger('click')
-        expect(wrapper.find('#modal').text()).toMatch('Add Notification')
-    });
-    it('should be added 1 new notification', async () => {
-        await wrapper.find('#show-btn').trigger('click')
-        wrapper.find('#name').setValue('Alfi')
-        wrapper.find('#add-btn').trigger('click')
-        expect(wrapper.vm.notifs.length).toBe(5)
-    });
-})
+  it('should have Notification as a title', () => {
+    expect(wrapper.text()).toMatch('Notification');
+  });
+  it('should open notification modal', async () => {
+    await wrapper.find('#show-btn').trigger('click');
+    expect(wrapper.find('#modal').text()).toMatch('Add Notification');
+  });
+  it('should be added 1 new notification', async () => {
+    await wrapper.find('#show-btn').trigger('click');
+    wrapper.find('#name').setValue('Alfi');
+    wrapper.find('#add-btn').trigger('click');
+    expect(wrapper.vm.notifs.length).toBe(4);
+  });
+});

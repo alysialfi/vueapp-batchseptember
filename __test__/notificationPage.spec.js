ac4992aedc8a71b1178f34ed337a7ea2ad66c15f
alysialfi <9c.alysialfi@gmail.com>
2024-01-13 13:39:41 +0700

    feat(notificationPage.scpe): test workflow

--- __test__/notificationPage.spec.js
+++ __test__/notificationPage.spec.js
@@ -5,7 +5,7 @@ describe('NotificationPage.vue Unit Test', () => {
   const wrapper = mount(NotificationPage);
 
   it('should have Notification as a title', () => {
-    expect(wrapper.text()).toMatch('Notification');
+    expect(wrapper.text()).toMatch('notification');
   });
   it('should open notification modal', async () => {
     await wrapper.find('#show-btn').trigger('click');

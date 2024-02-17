f1c6617d65a1eeab80d8f26c0f35ed317bdf89f0
alysialfi <9c.alysialfi@gmail.com>
2024-02-17 13:43:44 +0700

    fix(notificationPage.sepc.js): change title capital checking

--- __test__/notificationPage.spec.js
+++ __test__/notificationPage.spec.js
@@ -5,7 +5,7 @@ describe('NotificationPage.vue Unit Test', () => {
   const wrapper = mount(NotificationPage);
 
   it('should have Notification as a title', () => {
-    expect(wrapper.text()).toMatch('notification');
+    expect(wrapper.text()).toMatch('Notification');
   });
   it('should open notification modal', async () => {
     await wrapper.find('#show-btn').trigger('click');
